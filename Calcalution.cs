59aab40aefcb926a4adcee32545dd4a20041d8f6
Alex <94843696+AlexBor18@users.noreply.github.com>
2023-03-09 19:19:59 +0300

    Add files via upload

---
+++ Calcalution.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WSUniversalLib
+{
+    public class Calcalution
+    {
+        public static int GetQuantityForProduct(int productType, int materialType, int count, float width, float length)
+        {
+            double productkoeff;
+            double materialkoeff;
+            int square;
+            int quntity;
+            switch(productType)
+            {
+                case 1:
+                    productkoeff = 1.1;
+                    break;
+                case 2:
+                    productkoeff = 2.5;
+                    break;
+                case 3:
+                    productkoeff = 8.43;
+                    break;
+                default:
+                    return -1;
+            }
+            switch(materialType)
+            {
+                case 1:
+                    materialkoeff = 1.003;
+                    break;
+                case 2:
+                    materialkoeff = 1.0012;
+                    break;
+                default:
+                    return -1;
+            }
+
+            try
+            {
+                return (int)Math.Ceiling(productkoeff * materialkoeff * count * width * length);
+            }
+            catch(Exception)
+            {
+                return -1;
+            }
+        }
+    }
+}
